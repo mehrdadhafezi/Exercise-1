dda30edfbe82f764a0433c60037c6bc46d3dce2e
mehrdadhafezi <mhafezi98@gmail.com>
2022-03-11 13:25:01 +0330

    hh

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿for (var i = 0; i < 4; i++)
+{
+
+    for (var x = 4; x> i+1; x--)
+    {
+        System.Console.Write(" ");
+    }
+    for (var j = 0; j <= (i*2); j++)
+    {
+        System.Console.Write("*");
+    }
+    System.Console.WriteLine();
+}
+
